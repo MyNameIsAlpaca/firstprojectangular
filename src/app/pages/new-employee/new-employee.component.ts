a6d2da53061c9cfd666cbf5cfd11ece7ced249a5
MyNameIsAlpaca <saragosagabriele@gmail.com>
2023-11-17 15:44:44 +0100

    feat: denny

--- src/app/pages/new-employee/new-employee.component.ts
+++ src/app/pages/new-employee/new-employee.component.ts
@@ -2,19 +2,38 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { EmployeeCallsService } from '../../shared/crudhttp/employee-calls.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-new-employee',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,HttpClientModule],
   templateUrl: './new-employee.component.html',
-  styleUrl: './new-employee.component.scss'
+  styleUrl: './new-employee.component.scss',
+  providers: [EmployeeCallsService]
 })
 export class NewEmployeeComponent {
 
   newEmp: NewEmployee = new NewEmployee('','')
 
   newEmps: any[] = [];
+
+  employeesListServer: any[] = [];
+
+  constructor(private wService: EmployeeCallsService) {}
+
+  getEmployeeFromService(){
+    this.wService.getEmployeeData().subscribe({
+      next: (data: any) => {
+        this.employeesListServer = data;
+        console.log(this.employeesListServer)
+      },
+      error: (err: any) => {
+        console.log(err)
+      }
+    });
+  }
 
 InsertData(matricola: HTMLInputElement, nominativo: HTMLInputElement)
 {
@@ -35,8 +54,11 @@ insertSingleInput(ev: Event)
 
 }
 
-InsertDataForm()
+InsertDataForm(formEmployee: NgForm)
 {
+  this.newEmps.push(new NewEmployee(formEmployee.value.matricola, formEmployee.value.nominativo))
+  formEmployee.reset();
+  console.log(this.newEmps)
 }
 }
 
